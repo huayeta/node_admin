b14711ebfbdc8354f0652c4f31d82a9afedf94e2
huayeta <847457846@qq.com>
2021-08-29 13:29:15 +0800

    保存优化

--- src/tampermonkey/pro.js
+++ src/tampermonkey/pro.js
@@ -7,8 +7,9 @@ const json = JSON.parse(fs.readFileSync('json1.json'));
 const keys = Object.keys(json);
 let Text = '';
 keys.forEach((key,index)=>{
-    Text+=`${index+1}:${json[key].title}`;
-    Text+=`回答：${json[key].list.join('\n')}`;
+    if(index!==0)Text+='\n';
+    Text+=`${index+1}:${json[key].title}\n`;
+    Text+=`${json[key].list.map(text=>`    回答：${text}`).join('\n')}`;
 })
 fse.writeFile('json.text',Text);
 console.log('写入成功 json.text')
