5dc4206fa40279680a01719755ec252e50735087
huayeta <847457846@qq.com>
2022-01-16 17:34:34 +0800

    京东列表类目查询收集

---
+++ src/tampermonkey/see-jd.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         查看京东的列表数据
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  配合阿明工具箱
+// @author       You
+// @match        https://search.jd.com/Search?*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const jPrice = document.querySelector('#J_selectorPrice');
+    var eleLink = document.createElement('a');
+    eleLink.textContent='查看所有类目';
+    eleLink.addEventListener('click',function(){
+        // document.querySelector('.text-red.pointer').dispatchEvent(ev);
+        const spans = document.querySelectorAll('.text-red.pointer');
+        // Array.prototype.forEach.call(spans,span=>{
+        //     const ev = new Event('click',{"bubbles":true, "cancelable":false});
+        //     span.dispatchEvent(ev);
+        // })
+        const spanAs = Array.from(spans);
+        function clickSpan(){
+            if(spanAs.length>0){
+                const ev = new Event('click',{"bubbles":true, "cancelable":false});
+                const span = spanAs.splice(0,1)[0];
+                if(span.textContent.includes('点击获取')){
+                    span.dispatchEvent(ev);
+                    setTimeout(clickSpan,300);
+                }else{
+                    clickSpan();
+                }               
+            }
+        }
+        clickSpan();
+    },false)
+    jPrice.insertAdjacentElement('beforebegin',eleLink);
+    // Your code here...
+})();
