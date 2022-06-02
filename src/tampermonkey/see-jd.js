2f5ce39d67fbae0c1be23a3dfe26fe25b41f3fe0
huayeta <847457846@qq.com>
2022-06-03 00:50:23 +0800

    增强京东的查询类目

--- src/tampermonkey/see-jd.js
+++ src/tampermonkey/see-jd.js
@@ -36,5 +36,43 @@
         clickSpan();
     },false)
     jPrice.insertAdjacentElement('beforebegin',eleLink);
+    // 总结类目
+    const sjLink = document.createElement('a');
+    sjLink.style='margin-left:10px;';
+    sjLink.textContent='总结类目';
+    sjLink.addEventListener('click',()=>{
+        const uls = document.querySelectorAll('.dts-item');
+        const obj = {};
+        const arr = [];
+        Array.prototype.forEach.call(uls,(ul,index)=>{
+            // 去掉广告
+            if(index%6!==0){
+                const li = ul.querySelector('li:nth-child(4)');
+                let text = li.textContent;
+                // console.log(text);
+                if(!text.includes('点击查询')){
+                    text=text.slice(5);
+                    if(obj[text]){
+                        obj[text]++;
+                    }else{
+                        obj[text]=1;
+                    }
+                    if(arr.length===0){
+                        arr.push({[text]:1})
+                    }else{
+                        if(arr[arr.length-1][text]){
+                            arr[arr.length-1][text]++;
+                        }else{
+                            arr.push({[text]:1})
+                        }
+                    }
+                }
+            }
+        })
+        alert(JSON.stringify(obj)+'\n'+JSON.stringify(arr));
+        // console.log(obj);
+        // console.log(arr);
+    },false)
+    jPrice.insertAdjacentElement('beforebegin',sjLink);
     // Your code here...
 })();
