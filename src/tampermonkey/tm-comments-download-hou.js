70bbaaa30d7d13ce5d1b6412a880dc6296f9fad8
847457846@qq.com <847457846@qq.com>
2023-03-21 19:40:02 +0800

    增加查询违规入口

--- src/tampermonkey/tm-comments-download-hou.js
+++ src/tampermonkey/tm-comments-download-hou.js
@@ -5,6 +5,7 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://qn.taobao.com/home.htm/comment-manage/list/rateWait4PC?*
+// @match        https://myseller.taobao.com/home.htm/comment-manage/list/rateWait4PC?*
 // @require      https://stuk.github.io/jszip/dist/jszip.js
 // @grant        none
 // ==/UserScript==
