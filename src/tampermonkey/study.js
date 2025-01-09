dd32cf781af5e27c2392688dbf95ceb4fff722af
847457846@qq.com <847457846@qq.com>
2025-01-10 01:32:37 +0800

    添加分类

--- src/tampermonkey/study.js
+++ src/tampermonkey/study.js
@@ -1,11 +1,36 @@
-
 // 获取网址上面的query参数
 const Url = new URL(window.location.href);
 const QUERY = Url.searchParams.get('query') ? Url.searchParams.get('query') : '';
 const Disk = Url.searchParams.get('disk')? Url.searchParams.get('disk') : '';
 const BaseUrl = `${Url.origin}${Url.pathname}`;
 
+function addEventListener(el, eventName, eventHandler, selector) {
+    if (selector) {
+        const wrappedHandler = (e) => {
+            if (!e.target) return;
+            // console.log(e.target);
+            const el = e.target.closest(selector);
+            if (el) {
+                // console.log(el);
+                eventHandler.call(el, e);
+            }
+        };
+        el.addEventListener(eventName, wrappedHandler);
+        return wrappedHandler;
+    } else {
+        const wrappedHandler = (e) => {
+            eventHandler.call(el, e);
+        };
+        el.addEventListener(eventName, wrappedHandler);
+        return wrappedHandler;
+    }
+}
+
 const Tools = {
+    data:{
+      sel:null,
+      datas:[],
+    },
     formatBytes: (bytes) => {
         if (bytes === 0) return '0 B';
         const k = 1024;
@@ -49,11 +74,13 @@ const Tools = {
         const str = `
             <a href="${BaseUrl}" style="position:fixed;right:1em;bottom:1em;display:block;background:rgba(0,0,0,.2);padding:1em;width:2em;height:2em;border-radius:2em;line-height:2em;text-align:center;color:white;text-decoration:none;">主页</a>
             <h2>${QUERY}</h2>
+            ${!QUERY?`<div class="menu">${['语文','数学','英语'].map(data=>`<span ${(Tools.data.sel && Tools.data.sel.includes(data))?'class="sel"':''}>${data}</span>`).join('')}</div>`:''}
             <ul>
                 ${datas.map((data) => {
+                    if(Tools.data.sel && data.subject && !data.subject.includes(Tools.data.sel))return '';
                     let url = `?query=${encodeURIComponent((QUERY?`${QUERY}\\`:'') + data.file)}&disk=${data.disk}`;
                     if (data.type == "dir") {
-                        return `<li><a href="${url}">${data.file}</a></li>`;
+                        return `<li>${data.subject?`<span class="subject" style="${data.subject=='英语'?'color:aquamarine':''}">${data.subject}</span>`:''}<a href="${url}">${data.file}</a></li>`;
                     }
                     return `<li><a target="_black" href="/api/dir${url}">${data.file}</a><span class="bytes">${Tools.formatBytes(data.size)}</span></li>`;
         }).join('')}
@@ -64,7 +91,22 @@ const Tools = {
         Tools.addLoading();
         const datas = await Tools.readDir();
         if (datas) Tools.updataHtml(datas.datas);
+        Tools.data.datas = datas.datas;
         document.title = QUERY;
+        addEventListener(document.querySelector('.content'), 'click', (e) => {
+            const $target = e.target;
+            const sel = $target.textContent;
+            if(Tools.data.sel){
+                if(Tools.data.sel!==sel){
+                    Tools.data.sel = sel;
+                }else{
+                    Tools.data.sel = null;
+                }
+            }else{
+                Tools.data.sel = sel;
+            }
+            Tools.updataHtml(Tools.data.datas);
+        },'.menu span')
     }
 }
 Tools.initialization();
